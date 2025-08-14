1663a35a078466e4a39bc72a912a0b00cf82e004
Abdul Rafe <pakmtbers2003@gmail.com>
2025-08-15 02:13:04 +0500

    Fiew Updates

--- src/Components/Shared/dashboard.jsx
+++ src/Components/Shared/dashboard.jsx
@@ -1,27 +1,67 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
 const dashboard = () => {
-    const [selectUser, setSelectUser] = useState('');
+  const [id, setId] = useState(0);
+  const [role, setRole] = useState("");
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    const getrole = localStorage.getItem("role");
+    if (getrole) {
+      setRole(getrole);
+    }
+    const getname = localStorage.getItem("name");
+    if (getname) {
+      setName(getname);
+    }
+    const getusername = localStorage.getItem("username");
+    if (getusername) {
+      setUsername(getusername);
+    }
+    const getid = Number(localStorage.getItem("id"));
+    if (getid) {
+      setId(getid);
+    }
+  }, []);
+
 
   return (
     <div>
-        <h1>{selectUser} Dashboard</h1>
-        <div className="grid">
-            <div className="cards">
-                <h3 className="title">{ selectUser === "Teacher"? "My Students" : selectUser === "Student"? "Browse Teachers" : ""}</h3>
-            </div>
-            <div className="cards">
-                <h3 className="title">{ selectUser === "Teacher" || "Student" ? "View/Set Schedule" : "View Schedule"}</h3>
-            </div>
-            <div className="cards">
-                <h3 className="title">{ selectUser === "Parent" ? "Kid's ":""}Courses</h3>
-            </div>
-            <div className="cards">
-                <h3 className="title">{ selectUser === "Teacher" ? "Start Class" : selectUser === "Student" ? "Join Class" : "View Class"}</h3>
-            </div>
+      <h1>{name} Dashboard</h1>
+      <div className="grid">
+        <div className="cards">
+          <h3 className="title">
+            {role === "Teacher"
+              ? "My Students"
+              : role === "Student"
+              ? "Browse Teachers"
+              : "My Kids"}
+          </h3>
         </div>
+        <div className="cards">
+          <h3 className="title">
+            {role === "Teacher" || role === "Student"
+              ? "View/Set Schedule"
+              : "View Schedule"}
+          </h3>
+        </div>
+        <div className="cards">
+          <h3 className="title">
+            {role === "Parent" ? "Kid's " : ""}Courses
+          </h3>
+        </div>
+        <div className="cards">
+          <h3 className="title">
+            {role === "Teacher"
+              ? "Start Class"
+              : role === "Student"
+              ? "Join Class"
+              : "View Class"}
+          </h3>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default dashboard;
